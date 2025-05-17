6b81fc700f348a81520727f75313b56c609c2f73
ndimitrova <nninata@abv.bg>
2025-05-17 22:08:28 +0300

    Add C# project files

---
+++ Solution1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] numbers = { -42, -58, -50, -50 };
+
+CalculateAverage(numbers);
+
+
+ static double CalculateAverage(int[] numbers)
+{
+    int sum = 0;
+
+    foreach (int number in numbers)
+    {
+        sum += number;
+    }
+
+    return (double)sum / numbers.Length;
